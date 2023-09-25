a1d9705b5b7266cea03f27538201b72a207f8fdf
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-09-25 10:35:45 -0300

    feat: creating a location view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import {
+	requestForegroundPermissionsAsync,
+	getCurrentPositionAsync,
+	LocationObject,
+} from 'expo-location';
+
 import { View } from 'react-native';
 
 import { styles } from './styles';
 
 export default function App() {
+	const [location, setLocation] = useState<LocationObject | null>(null);
+
+	async function requestLocationPermissions() {
+		const { granted } = await requestForegroundPermissionsAsync();
+
+		if (granted) {
+			const currentPosition = await getCurrentPositionAsync();
+			setLocation(currentPosition);
+
+			console.log(currentPosition, 'current position');
+		}
+	}
+
+	useEffect(() => {
+		requestLocationPermissions();
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<></>
 		</View>
 	);
 }
--- styles.ts
+++ styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#daa',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
