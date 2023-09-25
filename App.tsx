c55c8500d1145f2f61d598ee759ca26ec80a1531
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-09-25 10:11:12 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
